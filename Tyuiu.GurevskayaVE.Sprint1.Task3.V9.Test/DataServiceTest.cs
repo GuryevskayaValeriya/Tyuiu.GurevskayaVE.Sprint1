54d470fcc6f10af17518e74554822dd6c78e4755
Tyuiu.GurevskayaVE@outlook.com <Tyuiu.GurevskayaVE@outlook.com>
2023-10-02 09:02:30 +0500

    task3

---
+++ Tyuiu.GurevskayaVE.Sprint1.Task3.V9.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GurevskayaVE.Sprint1.Task3.V9.Lib;
+
+namespace Tyuiu.GurevskayaVE.Sprint1.Task3.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckConvertMinutesToHours()
+        {
+            DataService Res = new DataService();
+            int minutes = 120;
+            var res = Res.ConvertMinutesToHours(minutes);
+
+            Assert.AreEqual(2, res);
+        }
+
+        [TestMethod]
+        public void CheckConvertMinutesToHours1()
+        {
+            DataService Res = new DataService();
+            int minutes = 120;
+            var res = Res.ConvertMinutesToHours1(minutes);
+
+            Assert.AreEqual(0, res);
+        }
+    }
+}
---
+++ Tyuiu.GurevskayaVE.Sprint1.Task3.V9/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GurevskayaVE.Sprint1.Task3.V9.Lib;
+
+namespace Tyuiu.GurevskayaVE.Sprint1.Task3.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнила: Гурьевская В.Е. | ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнила: Гурьевская В.Е. | ПКТб-23-2                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Написать программу пересчета величины временного интервала,             *");
+            Console.WriteLine("* заданного в минутах, в величину, выраженную в часах и минутах.          *");
+            Console.WriteLine("* Ответ округлите до 3 знаков после запятой.                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService Res = new DataService();
+
+            Console.Write("Введите временной интервал (в минутах) -> ");
+            int minutes = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(minutes + " минут" + " - это " + Res.ConvertMinutesToHours(minutes) + " ч. " + Res.ConvertMinutesToHours1(minutes) + " мин.");
+            Console.ReadKey();
+
+
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+        }
+    }
+}
